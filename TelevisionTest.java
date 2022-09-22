1d8384518a01facb03bf722abab8ee167997aa19
SanjanaSGhatina <114135590+SanjanaSGhatina@users.noreply.github.com>
2022-09-22 13:27:46 +0530

    Add files via upload

---
+++ TelevisionTest.java
@@ -0,0 +1,15 @@
+class TelevisionTest{
+public static void main(String a[]){
+System.out.println("Main method started");
+Television.brand="Boat";
+System.out.println("Television brand is :-"+Television.brand);
+Television.color="Black";
+System.out.println("Television color is :-"+Television.color);
+Television.price=50000.00;
+System.out.println("Television price is :-"+Television.price);
+Television.onandOff();
+Television.onandOff();
+System.out.println("Main method ended");
+}
+
+}
