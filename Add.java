cfeaf9706980dbc8d22457e7895c7072be3491a2
SanjanaSGhatina <114135590+SanjanaSGhatina@users.noreply.github.com>
2022-10-19 17:09:30 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,24 @@
+class Add{
+public static void main(String s[])
+{
+    add(56 , 78);
+	sub(658, 976);
+	mul(7594,943);
+	div(2,8);
+
+}
+public static void add(int a, int b){
+	System.out.println(a+b);
+}
+public static void sub(int a, int b){
+	System.out.println(a+b);
+}
+public static void mul(int a, int b){
+	System.out.println(a+b);
+}
+public static void div(int a, int b){
+	System.out.println(a*b);
+}
+
+
+}
---
+++ Prog.java
@@ -0,0 +1,9 @@
+class Prog{
+	public static void main(String ar[])
+	{
+		boolean isAlive=false;
+		long contactNo=212387562949L;
+		char ch='s';
+		System.out.println(ch);
+	}
+}
